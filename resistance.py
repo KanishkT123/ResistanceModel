860c13edbd2dffe373e10efbad77093a644553bb
Naysh <shyan.akmal@gmail.com>
2017-12-03 14:55:09 -0800

    Added lots of functions to compute supsicion updates. This is not tested. Also, only implemented right now to counter the simple spy strategy, wher we assume that spies will ALWAYS fail missions.

--- resistance.py
+++ resistance.py
@@ -7,6 +7,8 @@
 
 ## Imports ##
 from typing import Dict, List, Tuple
+from itertools import combinations
+from numpy import prod
 ## ####### ##
 
 ## Some Typing ##
@@ -70,42 +72,109 @@ class Resistance:
 				self.suspicion[player] = float(nSpies/(Resistance.nPlayers - 1))
 
 
-	# def updateSuspicion(self, voteResults: Dict[ID, Vote], spyStrat: str = "SIMPLE"):
-	# 	"""
-	# 	Given the observed votes in the round, and a list of IDs for the players
-	# 	who went on the mission, updates the player probability distribution
-	# 	using Bayes Rule. 
+	def updateSuspicion(self, voteResults: Dict[ID, Vote], spyStrat: str = "SIMPLE"):
+		"""
+		Given the observed votes in the round, and a list of IDs for the players
+		who went on the mission, updates the player probability distribution
+		using Bayes Rule. 
 
-	# 	Takes in a parameter to determine how exactly the likelihood should
-	# 	be calculated.
-	# 	"""
-	# 	# Holds posterior suspicions
-	# 	post: Dict[ID, float] = {}
+		Takes in a parameter to determine how exactly the likelihood should
+		be calculated.
+		"""
+		# Holds posterior suspicions
+		post: Dict[ID, float] = {}
 
-	# 	for player, prior in self.suspicion:
-	# 		# Compute likelihood for being a spy
-	# 		llhoodSpy: float = self.computeLikelihood(voteResults, spyStrat)
+		for player, prior in self.suspicion:
+			if player != self.name:
+				# Compute likelihood for being a spy
+				llhoodSpy: float = self.computeLikelihood(player, voteResults, spyStrat)
+				evidence: float = self.computeEvidence(voteResults, spyStrat)
 
-	# 		# Compute likelihood for being a resistance member
-	# 		llhoodResistance: float = 1
+				# Take the ratios scaled by the prior to get the posterior 
+				# probability of being a spy
+				rawPostSpy = llhoodSpy * prior
+				postSpy = rawPostSpy/evidence
+				post[player] = postSpy
 
-	# 		# Take the ratios scaled by the prior to get the posterior 
-	# 		# probability of being a spy
-	# 		rawPostSpy = llhoodSpy * prior
-	# 		evidence = rawPostSpy + (llhoodResistance * (1 - prior))
+		# Update probabilities at the very end
+		self.suspicion = {player:post[player] for player in post}
+		self.suspicion[self.name] = 0.0
 
-	# 		postSpy = rawPostSpy/evidence
-	# 		post[player] = postSpy
+	def computeEvidence(self, voteResults: Dict[ID, Vote], spyStrat: str):
+		playerList: List[ID] = list(self.suspicion.keys)
+		missionList: List[ID] = list(voteResults.keys)
 
-	# 	# Update probabilities
-	# 	self.suspicion = {player:post[player] for player in post}
+		nFailures = len([voteResults[player] == False for player in voteResults])
+		missionSize = len(voteResults)
+
+		nSpies = Resistance.nPlayers - Resistance.nResistance
+
+		evidence = 0.0
+		if spyStrat == "SIMPLE":
+			## In this strategy, we assume that the number of failures in the
+			## vote results is equal to the number of spies who went on the mission.
+			possWorlds = list(combinations(list(missionList), nFailures))
+
+			for world in possWorlds:
+				evidence += self.probAssignment(world, missionList)
+
+		return evidence
 
 
-	# def computeLikelihood(self, voteResults: Dict[ID, Vote], spyStrat: str):
+	def computeLikelihood(self, query: ID, voteResults: Dict[ID, Vote], spyStrat: str):
+		"""
+		Computes the likelihoods we would observe the input voteResults, given that
+		the query player was a spy or a resistance member respectively.
+		"""
+		playerList: List[ID] = list(self.suspicion.keys)
+		missionList: List[ID] = list(voteResults.keys)
+
+		nFailures = len([voteResults[player] == False for player in voteResults])
+		missionSize = len(voteResults)
+
+		nSpies = Resistance.nPlayers - Resistance.nResistance
+
+		llhood = 0.0
+		if spyStrat == "SIMPLE":
+			## In this strategy, we assume that the number of failures in the
+			## vote results is equal to the number of spies who went on the mission.
+			possWorlds = list(combinations(list(missionList), nFailures))
+
+			if (query in voteResults):
+				## Case where the query went on the mission
+				# Looking at all possible subsets of the people who went on the mission
+				# that could be spies
+				worldsOI = [x for x in possWorlds if query in x]
+				for world in worldsOI:
+					world.remove(query)
+
+				for world in worldsOI:
+					probWorld = self.probAssignment(world, missionList, query)
+					llhood += probWorld
+			else:
+				## Case where the query did not go on the mission
+				worldsOI = possWorlds
+				for world in worldsOI:
+					probWorld = self.probAssignment(world, missionList)
+					llhood += probWorld
+
+		return llhood
+	def probAssignment(self, spyList: List[ID], missionList: List[ID], ignore = False):
+		"""
+		Given a list of spy candidates from an overall list of people who
+		went on a mission, returns the probability exactly those candidates
+		are the spies on the mission.
+
+		Takes in an optional parameter ignore, which if equal to a player ID
+		will 
+		"""
+		probOfSpies =  prod([self.suspicion[candidate] for candidate in spyList])
+		probOfResistance = 1.0
+		for player in missionList:
+			if (player not in spyList) and (player != ignore):
+				probOfResistance = probOfResistance * (1 - self.suspicion[player])
 		
-	# 	if spyStrat == "SIMPLE":
-	# 		return 0
-
+		return probOfSpies * probOfResistance
 
 ## Temporary Testing ##
 
