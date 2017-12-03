745235ba17da103db19bad4b70d57c5cd55b5081
Naysh <shyan.akmal@gmail.com>
2017-12-03 13:51:13 -0800

    Added repr method.

--- resistance.py
+++ resistance.py
@@ -11,6 +11,7 @@ from typing import Dict, List, Tuple
 
 ## Some Typing ##
 ID = int
+Vote = bool
 ## ########### ##
 
 
@@ -35,12 +36,20 @@ class Resistance:
 		self.name = name
 		self.strat = strat
 
-		self.suspicion = {} # Initialized as an empty dictionary
+		self.suspicion: Dict[ID, float] = {} # Initialized as an empty dictionary
 
 		#! TODO: It might be silly to implement the below within
 		#!       the __init__ method
 		# Set knowledge of what the game looks like
 		Resistance.nPlayers, Resistance.nResistance = nGame
+
+	def __repr__(self):
+		rep = f"Resistance Member \
+		\n- ID: {self.name} \
+		\n- Strategy: {self.strat} \
+		\n- Suspicions: {str(self.suspicion)}"
+
+		return rep
 
 	def initSuspicion(self, playerList: List[ID]):
 		"""
@@ -49,24 +58,54 @@ class Resistance:
 
 		Should only be called ONCE at the very beginning of gameplay.
 		"""
+		nSpies = Resistance.nPlayers - Resistance.nResistance
+
 		for player in playerList:
 			if player == self.name:
 				# The player knows that they themselves are not a spy
 				self.suspicion[player] = 0.0
 			else:
 				# Uniform suspicion for all other players
-				self.suspicion[player] = float(1 - (Resistance.nResistance/Resistance.nPlayers))
+				# Note that we subtract one from the denominator
+				self.suspicion[player] = float(nSpies/(Resistance.nPlayers - 1))
 
 
-	def updateSuspicion(self):
-		"""
-		Given the observed votes in the round, and a list of IDs for the players
-		who went on the mission, updates the player probability distribution
-		using Bayes Rule. 
+	# def updateSuspicion(self, voteResults: Dict[ID, Vote], spyStrat: str = "SIMPLE"):
+	# 	"""
+	# 	Given the observed votes in the round, and a list of IDs for the players
+	# 	who went on the mission, updates the player probability distribution
+	# 	using Bayes Rule. 
 
-		Takes in a parameter to determine how exactly the likelihood should
-		be calculated.
-		"""
+	# 	Takes in a parameter to determine how exactly the likelihood should
+	# 	be calculated.
+	# 	"""
+	# 	# Holds posterior suspicions
+	# 	post: Dict[ID, float] = {}
+
+	# 	for player, prior in self.suspicion:
+	# 		# Compute likelihood for being a spy
+	# 		llhoodSpy: float = self.computeLikelihood(voteResults, spyStrat)
+
+	# 		# Compute likelihood for being a resistance member
+	# 		llhoodResistance: float = 1
+
+	# 		# Take the ratios scaled by the prior to get the posterior 
+	# 		# probability of being a spy
+	# 		rawPostSpy = llhoodSpy * prior
+	# 		evidence = rawPostSpy + (llhoodResistance * (1 - prior))
+
+	# 		postSpy = rawPostSpy/evidence
+	# 		post[player] = postSpy
+
+	# 	# Update probabilities
+	# 	self.suspicion = {player:post[player] for player in post}
+
+
+	# def computeLikelihood(self, voteResults: Dict[ID, Vote], spyStrat: str):
+		
+	# 	if spyStrat == "SIMPLE":
+	# 		return 0
+
 
 ## Temporary Testing ##
 
