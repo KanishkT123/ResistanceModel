c74dc2c4fb0bc0519a7d57dd3f2237b9cb4070f2
Naysh <shyan.akmal@gmail.com>
2017-12-03 12:09:19 -0800

    Began setting up skeleton for Resistance class.

---
+++ resistance.py
@@ -0,0 +1,21 @@
+#################
+## - Kanishk Tantia & Shyan Akmal
+## - 2017/12/1
+## - Discrete Math Modeling
+## - Final Project
+#################
+
+## Imports ##
+from typing import Dict
+## ####### ##
+
+
+## Resistance Class ##
+class Resistance:
+	def __init__(self, name, strat):
+		self.name = name
+		self.strat = strat
+
+
+
+## Temporary Testing ##
